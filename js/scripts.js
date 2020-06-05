e78b9b85ec2d00b73a931986ff7a8b499596472e
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-05 15:42:03 -0700

    add html, css and JS code for java language output

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,28 @@ const surveyResult = (lst) => {
   let js = 0;
   let py = 0;
   let csharp = 0;
+  let java = 0;
 
   for (i = 0; i < lst.length; i++) {
     if (lst[i] === "javascript") {
       js++;
     } else if (lst[i] === "python") {
       py++;
+    } else if (lst[i] === "java") {
+      java ++;
     } else {
       csharp++;
     }
   }
 
-  if (csharp > js && csharp > py) {
+  if (csharp > js && csharp > py && csharp > java) {
     return "csharp";
   }
-  else if (py > js && py > csharp) {
+  else if (py > js && py > csharp && py > java) {
     return "python";
+  }
+  else if (java > js && java > py && java > csharp) {
+    return "java";
   }
   else {
     return "javascript";
@@ -40,24 +46,35 @@ $(document).ready(function() {
     let result = surveyResult(resultArray);
 
     if (result === "javascript") {
-      $(".card-header").removeClass("bg-primary bg-indigo text-white").addClass("bg-javascript")
-      $(".btn").removeClass("bg-primary bg-indigo text-white").addClass("bg-javascript")
+      $(".card-header").removeClass("bg-primary bg-indigo text-white bg-java").addClass("bg-javascript")
+      $(".btn").removeClass("bg-primary bg-indigo text-white bg-java").addClass("bg-javascript")
       $(".py.hide").hide();
       $(".csharp.hide").hide();
+      $(".java.hide").hide();
       $(".js.hide").slideDown();
     }
     else if (result === "python") {
-      $(".card-header").removeClass("bg-javascript bg-primary bg-indigo").addClass("bg-python text-white")
-      $(".btn").removeClass("bg-javascript bg-primary bg-indigo").addClass("bg-python text-white")
+      $(".card-header").removeClass("bg-javascript bg-primary bg-indigo bg-java").addClass("bg-python text-white")
+      $(".btn").removeClass("bg-javascript bg-primary bg-indigo bg-java").addClass("bg-python text-white")
       $(".js.hide").hide();
       $(".csharp.hide").hide();
+      $(".java.hide").hide();
       $(".py.hide").slideDown();
     }
-    else {
-      $(".card-header").removeClass("bg-javascript bg-primary bg-python").addClass("bg-csharp text-white")
-      $(".btn").removeClass("bg-javascript bg-primary bg-python").addClass("bg-csharp text-white")
+    else if (result === "java") {
+      $(".card-header").removeClass("bg-javascript bg-primary bg-csharp bg-python text-white").addClass("bg-java text-white")
+      $(".btn").removeClass("bg-javascript bg-primary bg-python").addClass("bg-java text-white")
       $(".js.hide").hide();
       $(".py.hide").hide();
+      $(".csharp.hide").hide();
+      $(".java.hide").slideDown();
+    }
+    else {
+      $(".card-header").removeClass("bg-javascript bg-primary bg-python bg-java").addClass("bg-csharp text-white")
+      $(".btn").removeClass("bg-javascript bg-primary bg-python bg-java").addClass("bg-csharp text-white")
+      $(".js.hide").hide();
+      $(".py.hide").hide();
+      $(".java.hide").hide();
       $(".csharp.hide").slideDown();
     }
   });
